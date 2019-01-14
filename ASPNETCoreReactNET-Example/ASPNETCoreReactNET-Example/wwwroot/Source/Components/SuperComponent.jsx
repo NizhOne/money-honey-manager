0213294eea2aaf6150a130236195170971009796
github <suslovvladimir@outlook.com>
2019-01-14 23:54:05 +0200

    Add example

---
+++ ASPNETCoreReactNET-Example/ASPNETCoreReactNET-Example/wwwroot/Source/Components/SuperComponent.jsx
@@ -0,0 +1,11 @@
+﻿
+class SuperComponent extends React.Component {
+    render() {
+        return (
+            <div className="testDiv">
+                <h1>Test</h1>
+                <h3>Good</h3>
+            </div>
+        );
+    }
+}
